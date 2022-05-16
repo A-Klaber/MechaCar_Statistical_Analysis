b6242c82728b0d0d18baad7f9f9fc9318cac41de
A-Klaber <95887880+A-Klaber@users.noreply.github.com>
2022-05-16 04:34:16 -0400

    1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+# Import and read file as a dataframe
+mecha_mpg_data <- read.csv('MechaCar_mpg.csv')
+head(mecha_mpg_data)
+
+#Perform linear regression using the lm() function. 
+#In the lm() function, pass in all six variables (i.e., columns), and 
+#add the dataframe you created in Step 4 as the data parameter.
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg_data)
+
+#Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg_data))
+
+# import and read in the Suspension_Coil.csv file as a table
+suspension_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#total_summary dataframe using the summarize() function to get 
+#the mean, median, variance, and standard deviation of the suspension coil's PSI column
+total_summary <- suspension_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+
+#creates a lot_summary dataframe using the group_by() and the summarize() functions 
+#to group each manufacturing lot by the mean, median, variance, and standard deviation 
+#of the suspension coil's PSI column
+lot_summary <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep')
+lot_summary2 <- suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),Min=min(PSI),Max=max(PSI), .groups = 'keep')
+
+#using the t.test() function to determine if the PSI across all 
+#manufacturing lots is statistically different from the 
+#population mean of 1,500 pounds per square inch.
+t.test(log10(suspension_table$Manufacturing_Lot),mu=mean(log10(suspension_table$PSI))) 
+
+
+
+
+
+
+
+
